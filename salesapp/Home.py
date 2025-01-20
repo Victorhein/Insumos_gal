ca0ee8d8321a19dfd8166b6b50d11c1e505640ae
Victorhein <victorheinn@gmail.com>
2025-01-20 20:45:31 -0300

    Adicionando projeto

---
+++ salesapp/Home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.sidebar.image('Image/IVGI.png')
+
+st.markdown('# Bem-vindo ao Controle de Insumos da linha de Galvanização 2 ')
+
+st.divider()
+
+st.markdown(
+'''
+Projeto desenvolvido para maior eficiência no lançamento e controle de **insumos**.
+
+Sugestões podem ser enviadas para o e-mail: victor.hein@arcelormittal.com.br
+
+Web app desenvolvido pelo estagiário **Victor Hein**, em colaboração com a equipe de operação da GAL2
+'''
+)
